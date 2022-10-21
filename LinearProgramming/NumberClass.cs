ed2392047241de6ec042ca8f09c7141e28d6d7c5
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2022-10-21 13:47:04 +0300

    add NumberClass

---
+++ LinearProgramming/NumberClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace LinearProgramming
+{
+    public class NumberClass
+    {
+        private bool isNegative = false;
+        private double numerator;
+        private double detominator;
+
+        public NumberClass(string number)
+        {
+            if (number[0] == '-')
+            {
+                isNegative = true;
+            }
+
+            int indexOfSlash = number.IndexOf('/');
+
+            if (indexOfSlash < 0)
+            {
+                numerator = Convert.ToDouble(number);
+                detominator = 1;
+            }
+            else
+            {
+                string[] tempNumbers = number.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                numerator = Convert.ToDouble(tempNumbers[0]);
+                detominator = Convert.ToDouble(tempNumbers[1]);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (detominator == 1)
+            {
+                return numerator.ToString();
+            }
+            else
+            {
+                return numerator.ToString() + '/' + detominator.ToString();
+            }
+        }
+    }
+}
---
+++ LinearProgramming/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinearProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberClass number1 = new NumberClass("-123/23");
+            NumberClass number2 = new NumberClass("0");
+            NumberClass number3 = new NumberClass("10");
+            NumberClass number4 = new NumberClass("-4");
+            NumberClass number5 = new NumberClass("4/5");
+
+            Console.WriteLine(number1.ToString());
+            Console.WriteLine(number2.ToString());
+            Console.WriteLine(number3.ToString());
+            Console.WriteLine(number4.ToString());
+            Console.WriteLine(number5.ToString());
+        }
+    }
+}
